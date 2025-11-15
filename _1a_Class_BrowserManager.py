a23a20c37a926f1d0a0378e5a4e6158bc91c35dc
AleksandrBelous <ab21082001@mail.ru>
2025-11-15 23:54:51 +0300

    -

--- _1a_Class_BrowserManager.py
+++ _1a_Class_BrowserManager.py
@@ -2,6 +2,8 @@
 
 import undetected_chromedriver as uc
 
+CHROME_BINARY_PATH = "/usr/bin/google-chrome-stable"
+
 
 class BrowserManager:
     """
@@ -16,13 +18,19 @@ class BrowserManager:
         options = uc.ChromeOptions()
         prefs = { "profile.default_content_setting_values.geolocation": 2 }
         options.add_experimental_option("prefs", prefs)
+
+        # Добавляем аргументы для запуска в "безголовом" режиме на сервере
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
         self._options = options
 
     def __enter__(self) -> uc.Chrome:
         """Метод, вызываемый при входе в блок 'with'."""
 
         print("Запуск браузера...")
-        self.driver = uc.Chrome(options=self._options)
+        self.driver = uc.Chrome(options=self._options, browser_executable_path=CHROME_BINARY_PATH)
         return self.driver
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
